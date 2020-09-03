599b31636b11f96ec954bf3430c2d0334f3a06c7
tr4wzified <55751269+tr4wzified@users.noreply.github.com>
2020-09-03 21:10:19 +0200

    initial release

---
+++ ENBLightPatcher/Program.cs
@@ -0,0 +1,35 @@
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+
+namespace ENBLightPatcher
+{
+
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
+                args: args,
+                patcher: RunPatch,
+                new UserPreferences()
+                {
+                    ActionsForEmptyArgs = new RunDefaultPatcher()
+                    {
+                        IdentifyingModKey = "WaterDoesDamage.esp",
+                        TargetRelease = GameRelease.SkyrimSE
+                    }
+                }
+            );
+        }
+
+        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            foreach(var water in state.LoadOrder.PriorityOrder.WinningOverrides<Mutagen.Bethesda.Skyrim.IWaterGetter>())
+            {
+                var damageWater = state.PatchMod.Waters.GetOrAddAsOverride(water);
+                damageWater.Flags |= Mutagen.Bethesda.Skyrim.Water.Flag.CausesDamage;
+            }
+        }
+    }
+}
