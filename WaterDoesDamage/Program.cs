6bf73c8d488c868945bfdb04f0fa627fc478d3f2
Justin Swanson <justin.c.swanson@gmail.com>
2024-11-25 00:38:28 -0600

    nuget bump

--- WaterDoesDamage/Program.cs
+++ WaterDoesDamage/Program.cs
@@ -1,30 +1,28 @@
-using Mutagen.Bethesda;
-using Mutagen.Bethesda.Synthesis;
-using Mutagen.Bethesda.Skyrim;
 using System.Threading.Tasks;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
 
-namespace WaterDoesDamagePatcher
+namespace WaterDoesDamage;
+
+public class Program
 {
-    public class Program
+    public static Task<int> Main(string[] args)
     {
-        public static Task<int> Main(string[] args)
-        {
-            return SynthesisPipeline.Instance
-                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
-                .SetTypicalOpen(GameRelease.SkyrimSE, "WaterDoesDamagePatcher.esp")
-                .Run(args);
-        }
+        return SynthesisPipeline.Instance
+            .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+            .SetTypicalOpen(GameRelease.SkyrimSE, "WaterDoesDamagePatcher.esp")
+            .Run(args);
+    }
 
-        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+    public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+    {
+        foreach(var water in state.LoadOrder.PriorityOrder.WinningOverrides<IWaterGetter>())
         {
-            foreach(var water in state.LoadOrder.PriorityOrder.WinningOverrides<IWaterGetter>())
+            if (water.Flags != null && !water.Flags.Value.HasFlag(Water.Flag.CausesDamage))
             {
-                if (water.Flags != null && !water.Flags.Value.HasFlag(Water.Flag.CausesDamage))
-                {
-                    var damageWater = state.PatchMod.Waters.GetOrAddAsOverride(water);
-                    damageWater.Flags |= Mutagen.Bethesda.Skyrim.Water.Flag.CausesDamage;
-                }
-                else continue;
+                var damageWater = state.PatchMod.Waters.GetOrAddAsOverride(water);
+                damageWater.Flags |= Mutagen.Bethesda.Skyrim.Water.Flag.CausesDamage;
             }
         }
     }
